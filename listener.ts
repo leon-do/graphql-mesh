e7539e672bfb5796e0930789fdaefb52dfbcce57
leon-do <leondo123@gmail.com>
2024-09-16 14:50:29 -0400

    vercel

---
+++ listener.ts
@@ -0,0 +1,4 @@
+import { createBuiltMeshHTTPHandler } from './.mesh'
+
+// https://the-guild.dev/graphql/mesh/docs/getting-started/deploy-mesh-gateway#deploy-mesh-on-vercel-with-nextjs-api-routes
+export default createBuiltMeshHTTPHandler()
